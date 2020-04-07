4db474f036031118f661047c660e7b1da04a7d05
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-07 14:50:35 -0500

    added static folder

--- crm/settings.py
+++ crm/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRs = [
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
+]
